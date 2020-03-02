869f50ad1334fd081d475564da4a70e0acbe337d
Nihar Patil <nihar@menskin.co>
2020-03-02 16:29:49 -0500

    fixed redux inclusion

--- just-react/index.js
+++ just-react/index.js
@@ -1,33 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {createStore} from 'redux'
-import {Provider} from 'react-redux'
-
 import App from './components/App'
 
 
-const defaultState = [
-    'HW',
-    'squash',
-    'bottle'
-]
-
-const reducer = (state = defaultState, action) => {
-    const stateCopy = state.slice()
-    switch (action.type) {
-        case 'ADD_TODO_ITEM':
-            const newTodoItem = action.todoItem
-            stateCopy.push(newTodoItem)
-            break;
-    }
-    return stateCopy
-}
-
-const store = createStore(reducer)
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>,
+    <App/>
+    ,
     document.getElementById('react-root')
 )
